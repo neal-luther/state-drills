2d2603a7b0be50b9b5b3ab54f7c1f03b990a620d
g00dvibe <53553854+neal-luther@users.noreply.github.com>
2021-01-26 21:54:59 -0800

    Add files via upload

---
+++ Bomb.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class Bomb extends React.Component {
+  state = {
+    count: 0,
+    counter: ''
+  }
+
+  componentDidMount() {
+    const id = setInterval(() => {
+      this.setState({
+        count: this.state.count + 1
+      })      
+    }, 1000)
+
+    this.setState({
+      counter: id
+    })
+  }
+
+  componentWillMount() {
+    clearInterval(this.state.counter)
+  }
+
+  displayText() {
+    if (this.state.count >= 8) {
+      clearInterval(this.state.counter)
+      return 'BOOM!!!'
+    } else if (this.state.count % 2 === 0) {
+      return 'tick'
+    } else {
+      return 'tock'
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <p>
+          Count: {this.state.count}
+        </p>
+        <p>{this.displayText()}</p>
+      </div>
+    )
+  }
+}
+
+export default Bomb
---
+++ HelloWorld.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+    state = {
+        who: 'world'
+    }
+     handleClick = (e) => {
+         this.setState({
+             who: e.target.innerText
+         })
+     }
+
+    render(){
+        return(
+            <div>
+                <p>Hello {this.state.who}</p>
+                <button onClick={(e) => this.handleClick(e)}>World</button>
+                <button onClick={(e) => this.handleClick(e)}>Friend</button>
+                <button onClick={(e) => this.handleClick(e)}>React</button>
+            </div>
+        )
+    }
+}
+
+export default HelloWorld
